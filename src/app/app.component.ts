2c56a82db7741505a071f4f665a5be3b6cf17e00
Juampi27 <jpbarnetche27@gmail.com>
2022-01-11 12:20:22 -0300

    Directivas video 17 practica integradora Curso Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app_empleados';
+  titulo = 'Listado de Empleados';
+
+  empleados:Empleado[]=[   //click izquierdo sobre el error, aparece una lamparita azul, hacer click y t lleva a la solucion
+
+    new Empleado ("Juan", "Díaz", "Gerente", 40000),
+    new Empleado ("Carlos", "Gimenez", "Supervisor", 20000),
+    new Empleado ("Mariana", "Lopez", "Jefa de RRHH", 15000),
+    new Empleado ("Pabo", "Lavequia", "Operario", 10000),
+    new Empleado ("Marta", "Jauregui", "Encargada", 15000),
+
+  ];   
+
+  agregarEmpleado(){
+    
+    let miEmpleado = new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario);
+    this.empleados.push(miEmpleado);
+  
+  }
+
+  cuadroNombre:string = "";
+  cuadroApellido:string = "";
+  cuadroCargo:string = "";
+  cuadroSalario:number = 0;
+
+
 }
+
+
+
+
